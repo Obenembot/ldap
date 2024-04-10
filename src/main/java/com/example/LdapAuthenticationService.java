afbbca4c6d70d3782660173b3d3de0060eb5a5a6
sampson.obenembot <sampson.obenembot@sgits.co.za>
2024-04-10 16:41:21 +0200

    ldap confuguration

--- src/main/java/com/example/LdapAuthenticationService.java
+++ src/main/java/com/example/LdapAuthenticationService.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,8 @@ public class LdapAuthenticationService {
     private Logger log = LoggerFactory.getLogger(LdapAuthenticationService.class);
 
     private LdapTemplate ldapTemplate;
+    @Value("${spring.ldap.base}")
+    private String ldapBase;
     private PasswordEncoder passwordEncoder;
 
     public LdapAuthenticationService(final LdapTemplate ldapTemplate, PasswordEncoder passwordEncoder) {
@@ -19,17 +22,20 @@ public class LdapAuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public boolean userExists(String username) {
-        // You can customize this filter according to your LDAP structure
-        String filter = "(sAMAccountName=" + username + ")";
-        return ldapTemplate.authenticate("", filter, "");
-    }
 
     public boolean authenticate(String username, String password) {
         String encodePassword = passwordEncoder.encode(password);
         log.info("Authentication: {} {} ", password, encodePassword);
         // You can customize this filter according to your LDAP structure
         String filter = "(sAMAccountName=" + username + ")";
-        return ldapTemplate.authenticate("", filter, password);
+        boolean authenticate = ldapTemplate.authenticate(ldapBase, filter, password);
+        if (!authenticate) {
+            authenticate = ldapAuthenticate(username, password);
+        }
+        return authenticate;
+    }
+
+    private boolean ldapAuthenticate(String username, String password) {
+        return ldapTemplate.authenticate(ldapBase, "mail=" + username, password);
     }
 }
